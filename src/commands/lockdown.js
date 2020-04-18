3adecbd721d11a1706b8663d326b70db64277a1d
Emerick Bosch <xpc.dev@gmail.com>
2020-04-18 23:58:15 +0200

    Remove dummy message in command

--- src/commands/lockdown.js
+++ src/commands/lockdown.js
@@ -5,8 +5,7 @@ const command = {
     run: message => {
         const LOCKDOWN_DATE = DateTime.utc(2020, 03, 26, 21, 59, 59);
         const diff = DateTime.utc().diff(LOCKDOWN_DATE, ["days", "hours", "minutes", "seconds"]);
-        // message.channel.send(`South Africa has been in COVID-19 lockdown for **${diff.days} days**, **${diff.hours} hours** and **${diff.minutes} minutes**.`);
-        message.channel.send("uhuh");
+        message.channel.send(`South Africa has been in COVID-19 lockdown for **${diff.days} days**, **${diff.hours} hours** and **${diff.minutes} minutes**.`);
     },
     help: "Show how long we've been in lockdown."
 };
