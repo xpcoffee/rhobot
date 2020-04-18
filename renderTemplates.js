c8b78707dfc8f3431b8d628e4ecb451cc6efaf36
Emerick Bosch <xpc.dev@gmail.com>
2020-04-18 13:10:14 +0200

    Render the pull script from template during build
    This makes use of handlebars to populate dynamic data during the build step. The environment variables (aside from the docker image tag) need to be set up in CodeBuild.

---
+++ renderTemplates.js
@@ -0,0 +1,73 @@
+const Handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+
+const config = getConfig();
+info("Template rendering start")
+getTemplateFiles().forEach(renderTemplate);
+info("Template rendering complete")
+
+function getConfig() {
+    const environmentVariableMap = {
+        discordToken: "DISCORD_TOKEN",
+        awsAccountId: "AWS_ACCOUNT_ID",
+        region: "AWS_DEFAULT_REGION",
+        dockerImageTag: "DOCKER_IMAGE_TAG"
+    };
+
+    (function assertExpectedEnvironementVariables() {
+        const errors = Object.keys(environmentVariableMap).reduce((errors, key) => {
+            const environmentVariable = environmentVariableMap[key];
+            if (process.env[environmentVariable] === undefined) {
+                errors.push(`The ${environmentVariable} environment variable must be set`)
+            }
+            return errors;
+        }, []);
+
+        if (errors.length > 0) {
+            error(errors.join(" | "));
+        }
+    })();
+
+    return (function buildConfigFromEnvironmentVariables() {
+        return Object.keys(environmentVariableMap).reduce((config, key) => {
+            config[key] = process.env[environmentVariableMap[key]];
+            return config;
+        }, {});
+    })();
+}
+
+
+function getTemplateFiles() {
+    return [
+        "pull-docker-image.template",
+        "creds.yaml.template",
+    ];
+}
+
+function renderTemplate(filename) {
+    const TEMPLATE_DIRECTORY = "templates";
+    try {
+        const content = fs.readFileSync(path.join(TEMPLATE_DIRECTORY, filename), "utf8");
+        console.log(config);
+        const renderedContent = Handlebars.compile(content)(config);
+        const renderedFilename = stripTemplateSuffix(filename);
+        fs.writeFileSync(renderedFilename, renderedContent);
+    } catch (e) {
+        error(e);
+    }
+}
+
+function stripTemplateSuffix(filename) {
+    const index = filename.indexOf(".template");
+    return filename.slice(0, index);
+}
+
+function info(msg) {
+    console.log(`[INFO] ${msg}`);
+}
+
+function error(msg) {
+    console.error(`[ERROR] ${msg}`);
+    process.exit(1);
+}
