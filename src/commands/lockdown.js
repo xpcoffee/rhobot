031618b75339c711ff9c32bc7ea18ccb67bd9823
Emerick Bosch <xpc.dev@gmail.com>
2020-08-08 22:54:07 +0200

    Don't use an octal number
    Leading zeros cause JS to treat a number as an octal.
    In this case, this isn't intended.
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Deprecated_octal

--- src/commands/lockdown.js
+++ src/commands/lockdown.js
@@ -3,7 +3,7 @@ const DateTime = require("luxon").DateTime;
 // Return the time since the COVID-19 lockdown started in ZA.
 const command = {
     run: message => {
-        const LOCKDOWN_DATE = DateTime.utc(2020, 03, 26, 21, 59, 59);
+        const LOCKDOWN_DATE = DateTime.utc(2020, 3, 26, 21, 59, 59);
         const diff = DateTime.utc().diff(LOCKDOWN_DATE, ["days", "hours", "minutes", "seconds"]);
         message.channel.send(`South Africa has been in COVID-19 lockdown for **${diff.days} days**, **${diff.hours} hours** and **${diff.minutes} minutes**.`);
     },
